1cb6de3356353ab73f9826845a7bb6e9f4287b7f
Alok <mishraalok189381@gmail.com>
2025-03-11 17:08:51 +0530

    dashboard done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
               src={"/dashboard.png"}
               alt="Image"
               width={1920}
-              height={850}
+              height={860}
               // 1080
             ></Image>
           </NeonGradientCard>
