d04a8648cbf9f96499580eff3dbadf7be4b12ed2
Alok <mishraalok189381@gmail.com>
2025-03-11 17:15:45 +0530

    fixed footer

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -10,25 +10,19 @@ export default function MainPage(): React.ReactNode {
         <SheetData />
         <div className="mt-40 mx-3"></div>
       </div>
-      <div className="flex bg-black w-full h-auto justify-between">
+      <div className="flex bg-black w-full h-auto justify-center">
         
         <div className="text-2xl font-semibold text-white flex gap-3 mr-2 mb-2">
           <Link
-            href={"https://twitter.com/ayusssshhhhhhh"}
+            href={"https://x.com/Al0k_Mishra_"}
             className="underline hover:text-zinc-400 transition-colors duration-200"
             target="_blank"
           >
             <FaXTwitter />
           </Link>
+          
           <Link
-            href={"https://discord.gg/butxCZyntw"}
-            className="underline hover:text-zinc-400 transition-colors duration-200"
-            target="_blank"
-          >
-            <FaDiscord />
-          </Link>
-          <Link
-            href={"https://github.com/ayushsharma74"}
+            href={"https://github.com/Al0kKumar"}
             className="underline hover:text-zinc-400 transition-colors duration-200"
             target="_blank"
           >
