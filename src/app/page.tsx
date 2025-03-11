2a0536f2257b23e484f56bb759c6d6feaf7a11c6
Alok <mishraalok189381@gmail.com>
2025-03-11 17:06:33 +0530

    footer fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
               src={"/dashboard.png"}
               alt="Image"
               width={1920}
-              height={900}
+              height={850}
               // 1080
             ></Image>
           </NeonGradientCard>
